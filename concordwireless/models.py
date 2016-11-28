235f0143f92aabdca6b8422f251fa6bb8645784f
Pow Lee <Pow@Pows-Mac-mini.local>
2016-11-28 00:16:25 -0500

    started to add the models in the models.py in concordwireless folder. It is not finished yet

--- concordwireless/models.py
+++ concordwireless/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+'''
+class Phonerepair(models.Model):
+
+	employee = models.CharField(max_length = 50, blank = false)
+	date = 
+	firstname = 
+	lastname = 
+	contactnumber = 
+	email = 
+	phonemake = 
+	phonemodel = 
+	partreplacing = 
+	phonepassword = 
+	imei = 
+	quotedprice = 
+	technotes =
+
+'''
